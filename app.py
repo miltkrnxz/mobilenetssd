5541ac11595ffc6096e2ceee7e45908e117f1af3
miltkrnxz <89530333+miltkrnxz@users.noreply.github.com>
2021-09-29 13:40:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ UPLOAD_FOLDER ='static/uploads/'
 DOWNLOAD_FOLDER = 'static/downloads/'
 ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
 
-lineaccesstoken = ''
+lineaccesstoken = 'NRxuD82pF/rAiZ74N2OIPF0hbJ/3i+dkb3PJGkNEtxaMDt0Yjg/bh+VEHNvoioxLEkziOslCWhzkI7F5JAx2NINVEUNH16YU7wU3YpZWwto97WPa8ax5sEdxKRoz3DdY3Y9GvrlE4QWJn1gRdijBRAdB04t89/1O/w1cDnyilFU='
 
 line_bot_api = LineBotApi(lineaccesstoken)
 
